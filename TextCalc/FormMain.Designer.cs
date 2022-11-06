e30a51efa04cfac3845923f59f343cd40f1ab2ef
Vitaliy <mitradis@gmail.com>
2022-11-06 21:06:24 +0300

    Upload

---
+++ TextCalc/FormMain.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace TextCalc
+{
+    partial class FormMain
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(179, 140);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBoxs_TextChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxs_KeyDown);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxs_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(268, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(179, 140);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBoxs_TextChanged);
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxs_KeyDown);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxs_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(120, 158);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(219, 140);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(202, 69);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(54, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "+";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 310);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TextCalc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
+
---
+++ TextCalc/FormMain.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace TextCalc
+{
+    public partial class FormMain : Form
+    {
+        int operation = 0;
+        bool past = false;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != '+' && e.KeyChar != '-' && e.KeyChar != '*' && e.KeyChar != '/')
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == '+')
+            {
+                operation = 0;
+            }
+            else if (e.KeyChar == '-')
+            {
+                operation = 1;
+            }
+            else if (e.KeyChar == '*')
+            {
+                operation = 2;
+            }
+            else if (e.KeyChar == '/')
+            {
+                operation = 3;
+            }
+            calcFunc();
+        }
+
+        private void textBoxs_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (sender != null && e.Control)
+            {
+                if (e.KeyCode == Keys.A)
+                {
+                    ((TextBox)sender).SelectAll();
+                }
+                else if (e.KeyCode == Keys.V)
+                {
+                    past = true;
+                }
+            }
+        }
+
+        private void textBoxs_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                if (e.KeyChar == ',')
+                {
+                    e.KeyChar = '.';
+                }
+                if (e.KeyChar != '.')
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void textBoxs_TextChanged(object sender, EventArgs e)
+        {
+            calcFunc();
+        }
+
+        private void calcFunc()
+        {
+            textBox4.Clear();
+            if (textBox2.Text.Length > 0)
+            {
+                textBox2.TextChanged -= textBoxs_TextChanged;
+                if (past)
+                {
+                    past = false;
+                    textBox2.Text = textBox2.Text.Replace(",", ".").Replace("..", ".") + (textBox2.Lines[textBox2.Lines.Length - 1].Length > 0 ? Environment.NewLine : "");
+                }
+                else
+                {
+                    textBox2.Text = textBox2.Text.Replace(",", ".").Replace("..", ".");
+                }
+                textBox2.TextChanged += textBoxs_TextChanged;
+                textBox2.SelectionStart = textBox2.Text.Length;
+                textBox2.ScrollToCaret();
+            }
+            if (textBox3.Text.Length > 0)
+            {
+                textBox3.TextChanged -= textBoxs_TextChanged;
+                if (past)
+                {
+                    past = false;
+                    textBox3.Text = textBox3.Text.Replace(",", ".").Replace("..", ".") + (textBox2.Lines[textBox2.Lines.Length - 1].Length > 0 ? Environment.NewLine : "");
+                }
+                else
+                {
+                    textBox3.Text = textBox3.Text.Replace(",", ".").Replace("..", ".");
+                }
+                textBox3.TextChanged += textBoxs_TextChanged;
+                textBox3.SelectionStart = textBox3.Text.Length;
+                textBox3.ScrollToCaret();
+            }
+            if (textBox2.Text.Length > 0 && textBox3.Text.Length > 0)
+            {
+                List<string> tb1 = new List<string>();
+                tb1.AddRange(textBox2.Lines);
+                List<string> tb2 = new List<string>();
+                tb2.AddRange(textBox3.Lines);
+                int count1 = tb1.Count;
+                int count2 = tb2.Count;
+                for (int i = 0; i < count1 && i < count2; i++)
+                {
+                    if (!string.IsNullOrEmpty(tb1[i]) && !string.IsNullOrEmpty(tb2[i]))
+                    {
+                        if (tb1[i].Contains(".") || tb2[i].Contains(".") || operation == 3)
+                        {
+                            double value1 = -1;
+                            Double.TryParse(tb1[i], NumberStyles.Number, CultureInfo.InvariantCulture, out value1);
+                            double value2 = -1;
+                            Double.TryParse(tb2[i], NumberStyles.Number, CultureInfo.InvariantCulture, out value2);
+                            textBox4.Text = textBox4.Text + (operation == 0 ? (value1 + value2) : (operation == 1 ? (value1 - value2) : (operation == 2 ? (value1 * value2) : (value1 / value2)))).ToString().Replace(",", ".") + Environment.NewLine;
+                        }
+                        else
+                        {
+                            int value1 = -1;
+                            Int32.TryParse(tb1[i], out value1);
+                            int value2 = -1;
+                            Int32.TryParse(tb2[i], out value2);
+                            textBox4.Text = textBox4.Text + (operation == 0 ? (value1 + value2) : (operation == 1 ? (value1 - value2) : (value1 * value2))).ToString() + Environment.NewLine;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
