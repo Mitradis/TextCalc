3ca965db7a12efcd76ec95ff628c1170c38ea6c4
Vitaliy <mitradis@gmail.com>
2023-07-30 17:07:03 +0300

    clear

--- TextCalc/FormMain.Designer.cs
+++ TextCalc/FormMain.Designer.cs
@@ -103,8 +103,6 @@
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(459, 310);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -112,7 +110,6 @@
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
-            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
